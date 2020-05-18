77ce55d75de14f805fc5d3e0d01ec2dc7ce81880
horrjd1 <jamie489horrell@gmail.com>
2020-05-18 13:09:17 +1200

    Ajax returns api response

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 # good tutorial for flask:
 # https://www.tutorialspoint.com/flask/flask_url_building.htm
 
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, make_response
 from mongoengine import *
 import os
 import csv
+import json
+
 app = Flask(__name__)
 app.config.from_object('config')
 
@@ -15,60 +17,29 @@ app.config.from_object('config')
 
 connect('Web_3_db')
 
-# can create User objects; the argument Document passed into 
-# the constructor tells the system to map this class to a 
-# MongoDB document.
+   # can create User objects; the argument Document passed into 
+   # the constructor tells the system to map this class to a 
+   # MongoDB document.
 class User(Document):
-    email = StringField()
-    first_name = StringField()
-    last_name = StringField()
+   email = StringField()
+   first_name = StringField()
+   last_name = StringField()
 
 class Country(Document):
-    name = StringField()
-
-# creating a user
-# WARNING!!!!! THIS WILL RUN EVERYTIME THE SERVER IS STARTED ETC
-# jamie = User(first_name='Jamie', last_name='Horrell')
-# jamie.save()
-
-
-@app.route('/data_raw')
-def showData():
-   for file in os.listdir(app.config['FILES_FOLDER']):
-      filename = os.fsdecode(file)
-      path = os.path.join(app.config['FILES_FOLDER'],filename)
-      f = open(path)
-      r = csv.reader(f)
-      d = list(r)
-      for data in d:
-         print(data)
+   name = StringField()
+   abbreviation = StringField()
+   population = StringField()
 
 @app.route('/create_countries')
 def createCountries():
-   Country(name='Japan').save()
-   Country(name='New Zealand').save()
-   Country(name='Australia').save()
-   Country(name='US').save()
-   Country(name='Italy').save()
+   Country(name='Japan', abbreviation='JPN', population='126000000').save()
+   Country(name='New Zealand', abbreviation='NZ', population='5000000').save()
+   Country(name='Australia', abbreviation='AU', population='25000000').save()
+   Country(name='United States of America', abbreviation='USA', population='331000000').save()
 
    #return redirect('/countries')
 
-
-@app.route('/countries', methods=['GET'])
-@app.route('/countries/<country_name>', methods=['GET'])
-def getCountries(country_name = None):
-   if country_name == None:
-      return Country.objects.to_json()
-   else:
-      return Country.objects.get(name=country_name).to_json()
-
-# remember to go pip install requests
-@app.route('/countries', methods=['POST'])
-def addCountry():
-   name = request.form['name']
-   new_country = Country(name=name)
-   new_country.save()
-   return new_country.to_json()
+   
 #
 ###########################
 
@@ -80,7 +51,7 @@ def addCountry():
 @app.route('/home')
 def index():
    title = "SITE TITLE"
-   return render_template('index.html', title = title)
+   return render_template('index.html', title = title), 200
 
 @app.route('/inspiration')
 def inspiration():
@@ -90,6 +61,68 @@ def inspiration():
 def loadData():
    return 'Success'
 
+@app.route('/testing')
+def TestingPage():
+   return render_template('testing.html')
+
+# APIs #
+
+@app.route('/data_raw')
+def return_data():
+	dataList = []
+	for file in os.listdir(app.config['FILES_FOLDER']):
+		filename = os.fsdecode(file)
+		path = os.path.join(app.config['FILES_FOLDER'],filename)
+		f = open(path)
+		r = csv.reader(f)
+		d = list(r)
+		dataList.append(d)
+	return json.dumps(dataList)
+
+# Generic APIs for Checkpoints #
+
+@app.route('/api/countries', methods=['GET'])
+@app.route('/api/countries/<country_name>', methods=['GET'])
+def getCountries(country_name = None):
+   if country_name == None:
+      return Country.objects.to_json()
+   else:
+      countryJSON = Country.objects.get(name=country_name).to_json()
+      if len(countryJSON) == 0:
+         abort(404)
+
+      return countryJSON
+
+###### TEST THESE ONES ######
+
+@app.route('/api/countries', methods=['POST'])
+def add_country():
+   # make this line more efficient
+   if not request.json or not 'name' in request.json or not 'abbreviation' in request.json or not 'population' in request.json:
+      abort(400)
+   newName = request.json["name"]
+   newAbbreviation = request.json["abbreviation"]
+   newPopulation = request.json["population"]
+   Country(name=newName, abbreviation=newAbbreviation, population=newPopulation).save()
+   return redirect('/api/countries'), 201
+
+@app.route('/api/countries/<country_name>', methods=['DELETE'])
+def delete_country(country_name):
+   countryJSON = Country.objects.get(name=country_name).to_json()
+   if len(countryJSON) == 0:
+      abort(404)
+   country = Country.objects(name=country_name)
+   country.delete()
+
+
+
+
+# display 404 when abort(404)
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 
 
 
@@ -97,15 +130,15 @@ def loadData():
 
 ###########################
 #
-# returns your name on the page
-#
+   # returns your name on the page
+   #
 @app.route('/hello/<name>')
 def hello_name(name):
    return 'Hello %s!' % name
-#
-#
-# redirects
-#
+   #
+   #
+   # redirects
+   #
 @app.route('/admin')
 def hello_admin():
    return 'You are an Admin, good job!'
@@ -114,7 +147,7 @@ def hello_admin():
 def hello_guest(guest):
    return 'Hello guest %s' % guest
 
-# redirects if name is admin
+   # redirects if name is admin
 @app.route('/user/<name>')
 def hello_user(name):
    if name =='admin':
@@ -123,6 +156,15 @@ def hello_user(name):
       return redirect(url_for('hello_guest',guest = name))
 #
 ###########################
+
+
+
+
+# D3
+
+@app.route('/d3')
+def d3():
+   return render_template('d3.html')
 
 
 if __name__ =="__main__":
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,27 @@
-$(function(){ 
-
+$(document).ready(function () {
+    $.get('/api/countries', function (data) {
+        console.log("Ajax testing")
+        console.log(JSON.stringify(data))
+    }).fail(function () {
+        alert("No Data Found")
+    });
 });
+
+/*
+
+Get single item
+change <country name>
+
+$.get('/api/countries/<country_name>', function (data) {
+    console.log("Ajax testing")
+    console.log(JSON.stringify(data))
+}).fail(function () {
+    alert("No Data Found")
+});
+
+
+Post
+
+$.post()
+
+*/
